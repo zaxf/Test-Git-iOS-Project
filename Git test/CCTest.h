b3dc5d298225180094a0241ae24824053082f53f
zaxf <zaxf@zaxfs-Mac.local>
2014-03-25 23:36:29 +0300

    added atest class

---
+++ Git test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git test
+//
+//  Created by zaxf on ٢٥‏/٣‏/٢٠١٤.
+//  Copyright (c) ٢٠١٤ whatSoEver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git test
+//
+//  Created by zaxf on ٢٥‏/٣‏/٢٠١٤.
+//  Copyright (c) ٢٠١٤ whatSoEver. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
